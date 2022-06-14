d2a27e93be7db77c47fffc560947435072885f3a
Fanis Timerbulatov <TimerbulatovFT@gmail.com>
2022-06-14 10:42:42 +0500

    code update

--- dir/js/lesson_11clock_23/www/script.js
+++ dir/js/lesson_11clock_23/www/script.js
@@ -1,11 +1,13 @@
 /* Промисы */
+const url = document.currentScript.src;
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
   .then(json => {
     const printAnswer = Object.assign({},json);
     var myArray = Object.values(printAnswer);
-    var myString = JSON.stringify(printAnswer);
-    document.getElementById('content').innerHTML = `${myArray}<br>${myString}`;
+    var myString = JSON.stringify(printAnswer);    
+    document.getElementById('content').innerHTML = `<h1>Промисы</h1><br>${myArray}<br>${myString}<br><p style='font-weight: 400;font-size: 20px;'>Код скрипта:</p><iframe src=${url} scrolling="no" onload="resizeIframe(this)"></iframe>`;
+
 })
   .catch(error => console.error(error))
 
