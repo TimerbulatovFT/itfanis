253c83d40238bcb176f6c1c725e9e73e8d801276
Fanis Timerbulatov <TimerbulatovFT@gmail.com>
2022-06-17 22:28:11 +0500

    code update

--- dir/js/lesson_11clock_24/www/utils.js
+++ dir/js/lesson_11clock_24/www/utils.js
@@ -1,7 +1,7 @@
 /* Создаем функцию для создания запросов к серверу */
 const getData = (url) =>
 new Promise ((resolve, reject) =>
-fetch (url)
+fetch(url)
     .then(response => response.json())
     .then(json => resolve(json))
     .catch(error => reject(error))
