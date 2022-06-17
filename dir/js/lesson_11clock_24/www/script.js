79a9e33808a3dce13942d692b2e203581840182f
Fanis Timerbulatov <TimerbulatovFT@gmail.com>
2022-06-17 22:28:26 +0500

    code update

--- dir/js/lesson_11clock_24/www/script.js
+++ dir/js/lesson_11clock_24/www/script.js
@@ -1,7 +1,7 @@
 /* Использование функции созданной в файле utils.js */
 import getData from './utils.js'
 
-getData('https://jsonplaceholder.typicode.com/todos/1')
+getData('https://jsonplaceholder.typicode.com/todos/')
   .then(data => console.log(data))
   .catch(error => console.log(error.message))
 
